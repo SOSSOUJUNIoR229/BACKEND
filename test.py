6def350274425973c7dd198e066e689935871b38
SOSSOUJUNIoR229 <natanaelsossou@gmail.com>
2025-07-01 18:54:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 # Ouvrir la page dans le navigateur
-url = "http://127.0.0.1:5500/Frontend/index.html"  # ⚠️ Adapte le chemin selon ton projet
+url = "https://paiement-routier.vercel.app/index.html"  # ⚠️ Adapte le chemin selon ton projet
 webbrowser.open(url)
 
 print("✅ Page de paiement ouverte dans le navigateur.")
